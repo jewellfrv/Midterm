e5d788b19e7ff69a7c38ef1c17be6fdf8af30b56
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-22 23:18:45 -0400

    implementing specific commands into the calculator

---
+++ source/calculator_commands.py
@@ -0,0 +1,31 @@
+# src/calculator_commands.py
+from .commands import Command
+from .calculator import Calculator
+
+class AddCommand(Command):
+    def __init__(self, calculator: Calculator):
+        self.calculator = calculator
+
+    def execute(self, a: float, b: float) -> float:
+        return self.calculator.add(a, b)
+
+class SubtractCommand(Command):
+    def __init__(self, calculator: Calculator):
+        self.calculator = calculator
+
+    def execute(self, a: float, b: float) -> float:
+        return self.calculator.subtract(a, b)
+
+class MultiplyCommand(Command):
+    def __init__(self, calculator: Calculator):
+        self.calculator = calculator
+
+    def execute(self, a: float, b: float) -> float:
+        return self.calculator.multiply(a, b)
+
+class DivideCommand(Command):
+    def __init__(self, calculator: Calculator):
+        self.calculator = calculator
+
+    def execute(self, a: float, b: float) -> float:
+        return self.calculator.divide(a, b)
