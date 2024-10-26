bf9c44bce3f92172d671ef2a4029fcd891505119
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-25 21:43:00 -0400

    updated to add docstrings and another public method

--- source/plugins/add_plugin.py
+++ source/plugins/add_plugin.py
@@ -1,10 +1,17 @@
-# source/plugins/add_plugin.py
+# This module defines the AddPlugin class for performing addition in the calculator application
+
+
 from source.plugin_interface import Plugin
 from source.calculator import Calculator
 
 class AddPlugin(Plugin):
+    # A plugin class that performs addition for the calculator application
     def __init__(self, calculator: Calculator):
         self.calculator = calculator
 
-    def execute(self, a: float, b: float) -> float:
+    def execute(self, a: float, b: float, *args) -> float:
         return self.calculator.add(a, b)
+    
+    def get_description(self):
+        # Returns a description of the plugin's purpose
+        return "This plugin performs addition."
