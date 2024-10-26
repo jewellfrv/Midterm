559006cf336e9c15ac19621d467ff410922c3706
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-25 23:09:25 -0400

    removing because file moved under source folder

--- source/plugins/plugin_interface.py
+++
@@ -1,6 +0,0 @@
-# source/plugin_interface.py
-
-class Plugin:
-    def execute(self, *args):
-        """Execute the plugin command."""
-        raise NotImplementedError("This method should be overridden.")
