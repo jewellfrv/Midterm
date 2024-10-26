1b467bb266ddf1834aeec5f1c3d849adbca075b6
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-25 21:45:56 -0400

    updated to add doctrings and another public method

--- source/plugins/multiply_plugin.py
+++ source/plugins/multiply_plugin.py
@@ -1,10 +1,16 @@
-# source/plugins/multiply_plugin.py
+# This module defines the MultiplyPlugin class for performing multiplication in the calculator application
+
 from source.plugin_interface import Plugin
 from source.calculator import Calculator
 
 class MultiplyPlugin(Plugin):
+    # A plugin class that performs multiplication for the calculator application
     def __init__(self, calculator: Calculator):
         self.calculator = calculator
 
-    def execute(self, a: float, b: float) -> float:
+    def execute(self, a: float, b: float, *args) -> float:
         return self.calculator.multiply(a, b)
+
+    def get_description(self):
+        # Returns a description of the plugin's purpose
+        return "This plugin performs multiplication."
